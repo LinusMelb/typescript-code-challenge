b9ebfd4625ed7bcfb7414e414832ae49b62deb9a
Linus Peng <penxumelb@gmail.com>
2021-01-28 18:51:17 +1100

    ts setup

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    coverageDirectory: 'coverage',
+    collectCoverage: true,
+    transform: {
+      '^.+\\.ts?$': 'ts-jest'
+    },
+    testMatch: [
+      '**/test/**/*.ts'
+    ],
+    verbose: true,
+    collectCoverageFrom: [
+      '**/src/**/*.ts'
+    ],
+    testPathIgnorePatterns: [
+      'dist.*\\.ts$'
+    ],
+  }
